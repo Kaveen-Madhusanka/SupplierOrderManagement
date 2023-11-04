95dffca089bf23e23df411fb13a0d4943f3d48dd
Kaveen <kaveen.madusanka1@gmail.com>
2023-11-05 01:02:45 +0530

    DB context modifications

--- SOMSystem/SOM.ProductService.Infastructure/Persistant/AppDbContext.cs
+++ SOMSystem/SOM.ProductService.Infastructure/Persistant/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SOM.ProductService.Application.Common.Interface;
+using SOM.ProductService.Domain;
 using SOM.ProductService.Domain.Product;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,34 @@ namespace SOM.ProductService.Infastructure.Persistant
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "Kaveen";
+                        entry.Entity.Created = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "Kaveen";
+                        entry.Entity.LastModified = DateTime.Now;
+                        break;
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+            return result;
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
