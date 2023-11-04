825ff3bd79946bb9659af87a4ebb1bc9430c65ca
Kaveen <kaveen.madusanka1@gmail.com>
2023-11-04 23:57:26 +0530

    Add ef core and setup db context

---
+++ SOMSystem/SOM.ProductService.Infastructure/Persistant/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SOM.ProductService.Application.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.ProductService.Infastructure.Persistant
+{
+    public class AppDbContext: DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- SOMSystem/SOM.ProductServiceApi/Program.cs
+++ SOMSystem/SOM.ProductServiceApi/Program.cs
@@ -1,6 +1,9 @@
+using SOM.ProductService.Infastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
