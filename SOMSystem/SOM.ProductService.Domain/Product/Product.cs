919ffe2e3226793a78c15d6486a7cf73d7d264bb
Kaveen <kaveen.madusanka1@gmail.com>
2023-11-05 00:33:34 +0530

    add product domain and configurations

---
+++ SOMSystem/SOM.ProductService.Domain/Product/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.ProductService.Domain.Product
+{
+    public class Product : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string? Description { get; set; }
+        public int? ProductCategoryId { get; set; }
+        public ProductCategory ProductCategory { get; set; } = null!;
+    }
+}
