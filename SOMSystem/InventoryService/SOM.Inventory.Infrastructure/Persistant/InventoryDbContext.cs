6d6f53a304527a1b839bac4a94eaf025c7d0dcdd
ChathurangaKumara <ChathurangaKumara@DESKTOP-IMT4SVA>
2023-12-09 23:52:51 +0530

    add inventory service

---
+++ SOMSystem/InventoryService/SOM.Inventory.Infrastructure/Persistant/InventoryDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SOM.Inventory.Application.Common.Interface;
+using SOM.Shared.Entities;
+using System.Reflection;
+
+namespace SOM.Inventory.Infrastructure.Persistant
+{
+    public class InventoryDbContext : DbContext, IInventoryDbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
+        {
+
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "Kaveen";
+                        entry.Entity.Created = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "Kaveen";
+                        entry.Entity.LastModified = DateTime.UtcNow;
+                        break;
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }  
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+            return result;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
