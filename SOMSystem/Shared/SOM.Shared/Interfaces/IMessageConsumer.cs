9f5eb87e4a82c765565c6db98394da525a2a75b4
Kaveen <kaveen.madusanka1@gmail.com>
2023-12-23 14:03:15 +0530

    Basic supplier queue consumer in product service

---
+++ SOMSystem/Shared/SOM.Shared/Interfaces/IMessageConsumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.Shared.Interfaces
+{
+    public interface IMessageConsumer
+    {
+        void Receive(string publisherName, string exchangeName, string queueName, string routingKey);
+    }
+}
